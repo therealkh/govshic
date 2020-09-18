4d871a7e0af71f7a9f3259c3e2856661c7503d1c
therealkh <uktam4ik.khamidov@gmail.com>
2020-09-18 13:08:59 +0500

    первый экран готов, слайдер сделал

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,6 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const socials = headerWrapper.querySelector('.header-socials');
   const lang = headerWrapper.querySelector('.header-lang');
 
+  const btnAbout = document.querySelector('.info__about');
+  const btnBackAbout = document.querySelector('.about-back');
+  const about = document.querySelector('.main-about');
+
+  const mainWrapper = document.querySelector('.main__wrapper');
+
   let lastDocWidth;
   lastDocWidth = window.innerWidth;
 
@@ -19,6 +25,18 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       else {
         CloseBurger();
+      }
+    }
+    if (event.target === btnAbout) {
+      event.preventDefault();
+      if (!about.classList.contains('shown')) {
+        OpenAbout();
+      }
+    }
+    if (event.target.closest('.about-back')) {
+      event.preventDefault();
+      if (about.classList.contains('shown')) {
+        CloseAbout();
       }
     }
   });
@@ -51,6 +69,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   //! -----------------[Functions]-------------
+  function OpenAbout() {
+
+    let mainWrapperChildren = mainWrapper.children;
+    for (let i = 0; i < mainWrapperChildren.length; i++) {
+      if (mainWrapperChildren[i] != about) {
+        mainWrapperChildren[i].style.opacity = 0;
+      }
+    }
+    about.style.display = 'flex';
+    let aboutHeight = about.scrollHeight;
+    mainWrapper.style.height = `${aboutHeight}px`;
+    setTimeout(() => {
+      about.classList.add('shown');
+    }, 1);
+  }
+  function CloseAbout() {
+    about.classList.remove('shown');
+    mainWrapper.style.height = '';
+    let mainWrapperChildren = mainWrapper.children;
+    for (let i = 0; i < mainWrapperChildren.length; i++) {
+      //mainWrapperChildren[i].style.visibility = '';
+      mainWrapperChildren[i].style.opacity = 1;
+    }
+
+    setTimeout(() => {
+      about.style.display = 'none';
+    }, 300);
+  }
   function OpenBurger() {
     let headerHeight = headerWrapper.scrollHeight;
     menu.style.marginTop = '0';
